979b1d119e58fb3a5f8f91d19566859992c8a2c7
LEON LAVATSA <77143585+Lavatsaleo@users.noreply.github.com>
2026-09-03 16:59:54 +0100

    Add canonical Physical Academy consent text

---
+++ src/data/physicalAcademyConsent.js
@@ -0,0 +1,56 @@
+const PHYSICAL_ACADEMY_CONSENT_VERSION = "DF-01-PHYSICAL-2026-09-03";
+
+const PHYSICAL_ACADEMY_CONSENT = {
+  version: PHYSICAL_ACADEMY_CONSENT_VERSION,
+  title: "Digital Futures Programme - Participant Application Consent",
+  introductionTitle: "Introduction",
+  introduction: [
+    "Dear Applicant,",
+    "The Digital Futures programme collects and analyses participants’ information to improve the programme activities and ensure they have a positive impact. We value your privacy and are committed to protecting your personal data. This form explains how your information will be used and shared. Please read it carefully before signing.",
+  ],
+  purposeTitle: "Purpose of Data Collection and Use:",
+  purpose: [
+    "We will collect your data for eligibility assessment, selection into the programme and feedback for programme improvement. This may include your name, location, age, gender, contact details and other relevant information. There is no payment or application fee. Your information will be kept private, secure, and shared only with approved project partners and data processors.",
+    "Taking part is your choice, and you may ask us to delete your information at any time.",
+    "You will also receive a copy of this form.",
+  ],
+  informationLinks: [
+    {
+      before: "For more information, see ",
+      label: "Sightsavers' Data Protection Policy",
+      url: "https://sightsavershh.sharepoint.com/Policy%20Library/Forms/AllItems.aspx?id=%2FPolicy%20Library%2FData%20Protection%20Policy%2Epdf&parent=%2FPolicy%20Library",
+      between: " and the Foundation’s privacy statement at ",
+      secondLabel: "https://mastercardfdn.org/privacy/",
+      secondUrl: "https://mastercardfdn.org/privacy/",
+    },
+  ],
+  rightsTitle: "Your Rights:",
+  rights: [
+    "You have the right to access, correct, delete, restrict, object to the use of, or request transfer of your personal data. You have the right to be treated fairly and with respect by everyone involved in this project. If you have any concerns about someone's behaviour, please contact the Country Safeguarding Lead (Respective country safeguarding lead) or use the Speak Up platform: ",
+  ],
+  speakUpUrl: "https://www.sightsavers.org/how-were-run/accountability-and-transparency/speakup/",
+  questionsTitle: "Questions or concerns",
+  questions: [
+    "If you have any questions about the application form, kindly contact: (Respective MEL and Programme managers)",
+  ],
+  consentTitle: "Your Consent",
+  consentIntro: "By proceeding to complete this questionnaire, you confirm that:",
+  consentBullets: [
+    "you have read and understood this information (or had it explained to you)",
+    "you have had the opportunity to ask questions",
+    "you understand that taking part is voluntary and that you may stop at any time",
+    "you agree to take part",
+  ],
+  consentStatements: [
+    "I have read and understood this information",
+    "I agree to take part in this questionnaire",
+  ],
+  juratTitle: "JURAT CLAUSE",
+  juratWhen: "(To be used where the participant does not speak, read, or write English, or has a disability requiring the form to be translated or explained):",
+  juratClause: "“I [name] of [address] have distinctly, clearly, and audibly interpreted/explained the above in the [name of language] language/dialect to the abovenamed person who seemed to clearly understand the above and who made his/her mark in my presence\".",
+};
+
+module.exports = {
+  PHYSICAL_ACADEMY_CONSENT_VERSION,
+  PHYSICAL_ACADEMY_CONSENT,
+};
